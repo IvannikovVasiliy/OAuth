c0a05b4cba15a4ed9d9a2cc316d8553a641a723f
IvannikovVasiliy <ivannikovvasiliya2001@gmail.com>
2022-11-17 15:45:18 +0300

    first commit

---
+++ resourceserver/src/main/java/com/ivannikov/resourceserver/ResourceServer.java
@@ -0,0 +1,12 @@
+package com.ivannikov.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceServer.class, args);
+    }
+}
